8ce0ce04127b252480ba99cafe5eeaa6f40d733c
Richard van den Broek <vandenbroek.rp@gmail.com>
2019-04-30 18:02:10 +0200

    roughly styling homepage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,37 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [ :show, :update ]
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = @customer.items.new(item_params)
+    @item.customer = current_customer
+
+    if @item.save
+      redirect_to items_path
+    else
+      render "item_items/index"
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path
+  end
+
+private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,92 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_04_29_124550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.jsonb "corr_address"
+    t.string "vat_number"
+    t.jsonb "bank_details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.jsonb "address"
+    t.string "phone"
+    t.bigint "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_customers_on_company_id"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.integer "amount"
+    t.jsonb "invoice_address"
+    t.string "filepath"
+    t.bigint "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_invoices_on_order_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "model"
+    t.text "specs"
+    t.string "image_url"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "number"
+    t.string "status"
+    t.jsonb "delivery_address"
+    t.jsonb "invoice_address"
+    t.bigint "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+    t.index ["number"], name: "index_orders_on_number", unique: true
+  end
+
+  add_foreign_key "customers", "companies"
+  add_foreign_key "invoices", "orders"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "customers"
 end
