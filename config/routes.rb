b8f37fadf2e9a16cd5f3a0fd122f43ab50d2bc91
Richard van den Broek <vandenbroek.rp@gmail.com>
2019-04-30 15:57:01 +0200

    building outline for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
-  get 'company/create'
-  get 'company/read'
-  get 'company/update'
-  get 'company/destroy'
   devise_for :customers
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
